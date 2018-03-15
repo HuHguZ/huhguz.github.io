b1f4c1eb57aea58b74aad48c63457cdc8910d863
HuHguZ <huhguz@mail.ru>
2018-03-15 22:48:39 +0300

    kdf

--- NickNameGenerator/script.js
+++ NickNameGenerator/script.js
@@ -1,24 +1,52 @@
 window.addEventListener('load', function() {
     var how = document.getElementById('how'),
         res = document.getElementById('res'),
+        clk = document.getElementById('clk'),
         flag = true,
         counter1 = -1,
         counter2 = 0,
         ctrnick = '',
+        colors = {
+            rgb1: [Math.random() * 256 ^ 0, Math.random() * 256 ^ 0, Math.random() * 256 ^ 0],
+            rgb2: [Math.random() * 256 ^ 0, Math.random() * 256 ^ 0, Math.random() * 256 ^ 0],
+            c1: Math.random() * 3 ^ 0,
+            c2: Math.random() * 3 ^ 0,
+            mul1: 1,
+            mul2: 1,
+            rndrgb: function(array, channel, multiplier) {
+                array[channel] += multiplier;
+            },
+            randomize: function() {
+                this.rndrgb(this.rgb1, this.c1, this.mul1);
+                this.rndrgb(this.rgb2, this.c2, this.mul2);
+                for (var i = 1; i < 3; i++) {
+                    if (this[`rgb${i}`][this[`c${i}`]] > 255) {
+                        this[`c${i}`] = Math.random() * 3 ^ 0;
+                        this[`mul${i}`] *= -1;
+                    } else if (this[`rgb${i}`][this[`c${i}`]] < 0) {
+                        this[`c${i}`] = Math.random() * 3 ^ 0;
+                        this[`mul${i}`] *= -1;
+                    }
+                }
+            }
+        },
         func;
     how.value = (Math.random() * 5 ^ 0) + 4;
     nickName();
 
     function nickName() {
         var name = '',
-            str1, str2, f, y, fl, ff, q, member = ["a", "e", "i", "o", "u", "y", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
+            str1, str2, f, y, fl, ff, q, member = ["a", "e", "i", "o", "u", "y", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"], counter = +how.value;
         ff = Math.round(Math.random() * 1);
         if (ff == 1) {
             fl = true;
         } else {
             fl = false;
         }
-        for (var i = 1; i <= +how.value; i++) {
+        for (var i = 1; i <= counter; i++) {
+            if ((Math.random() * 20 ^ 0) < 3) {
+                fl = !fl;
+            }
             if (fl) {
                 fl = !fl;
                 q = member[Math.round(Math.random() * 5)];
@@ -73,14 +101,12 @@ window.addEventListener('load', function() {
             }
         }, 1);
     }
-    document.getElementById('clk').addEventListener('click', function() {
-            // this.style.background = `linear-gradient(to top left, ${getRndColor()}, ${getRndColor()})`;
-
+    clk.addEventListener('click', function() {
         if (+how.value && flag) {
             nickName();
         }
     });
-    // document.getElementById('clk').style.background = `linear-gradient(to top left, ${getRndColor()}, ${getRndColor()})`;
+
     function getRndColor() {
         var a, b = "#",
             color = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
@@ -90,4 +116,8 @@ window.addEventListener('load', function() {
         }
         return b;
     }
+    setInterval(function() {
+        colors.randomize();
+        clk.style.background = `linear-gradient(to top left, rgb(${colors.rgb1[0]}, ${colors.rgb1[1]}, ${colors.rgb1[2]}), rgb(${colors.rgb2[0]}, ${colors.rgb2[1]}, ${colors.rgb2[2]}))`;
+    }, 10);
 });
