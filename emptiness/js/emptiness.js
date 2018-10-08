d722127fe5a1968064672da29325b541b8eddc33
HuHguZ <huhguz@mail.ru>
2018-10-08 17:16:49 +0300

    EMPTINESS
    EMPTINESS

---
+++ emptiness/js/emptiness.js
@@ -0,0 +1,38 @@
+﻿window.addEventListener(`load`, () => {
+    let getElem = el => document.getElementById(el),
+        data = getElem(`data`),
+        out = getElem(`out`),
+        encode = getElem(`encode`),
+        decode = getElem(`decode`),
+        symbols = [
+            8204, // нулевой бит
+            8203, // единичный бит
+        ],
+        separator = 8205;  // разделитель двоичных цифр
+    encode.addEventListener(`click`, () => { //Шифруем и копируем в буффер обмена
+        let r = ``,
+            sep = String.fromCharCode(separator);
+        for (let i = 0; i < data.value.length; i++) {
+            r += `${data.value[i].charCodeAt(0).toString(2).split(``).map(e => String.fromCharCode(symbols[+e])).join(``)}${i == data.value.length - 1 ? `` : sep}`;
+        }
+        out.value = r || `Нечего зашифровывать`;
+        if (r) {
+            out.select();
+            document.execCommand('copy');
+            window.getSelection().removeAllRanges();
+        }
+    });
+    decode.addEventListener(`click`, () => {
+        let r = ``,
+            syms = out.value.split(String.fromCharCode(separator));
+        for (let i = 0; i < syms.length; i++) {
+            r += String.fromCharCode(parseInt(syms[i].split(``).map(e => e.charCodeAt(0) == symbols[1] ? `1` : `0`).join(``), 2));
+        }
+        data.value = r || `Нечего расшифровывать`;
+        if (r) {
+            data.select();
+            document.execCommand('copy');
+            window.getSelection().removeAllRanges();
+        }
+    });
+});
--- grouping/js/grouping.js
+++ grouping/js/grouping.js
@@ -1,128 +1,131 @@
 ﻿window.addEventListener('load', () => {
-            let getElem = el => document.getElementById(el),
-                data = getElem(`data`),
-                data2 = getElem(`data2`),
-                groups = getElem(`groups`),
-                start = getElem(`start`),
-                accuracy = getElem(`accuracy`),
-                minobs = getElem(`minobs`),
-                out = getElem(`out`),
-                input = getElem(`input`),
-                showInitialData = getElem(`showInitialData`),
-                tablex2 = [3.84, 5.99, 7.82, 9.49, 11.07, 12.59, 14.07, 15.51, 16.92, 18.31, 19.68, 21, 22.4, 23.7, 25, 26.3, 27.6, 28.9, 30.1, 31.4, 32.7, 33.9, 35.2, 36.4, 37.7, 38.9, 40.1, 41.3, 42.6, 43.8], //p = 0.05
-                checkValue = num => function() {
-                    this.value = +this.value <= num ? num : this.value;
+    let getElem = el => document.getElementById(el),
+        data = getElem(`data`),
+        data2 = getElem(`data2`),
+        groups = getElem(`groups`),
+        start = getElem(`start`),
+        accuracy = getElem(`accuracy`),
+        minobs = getElem(`minobs`),
+        out = getElem(`out`),
+        input = getElem(`input`),
+        showInitialData = getElem(`showInitialData`),
+        tablex2 = [3.84, 5.99, 7.82, 9.49, 11.07, 12.59, 14.07, 15.51, 16.92, 18.31, 19.68, 21, 22.4, 23.7, 25, 26.3, 27.6, 28.9, 30.1, 31.4, 32.7, 33.9, 35.2, 36.4, 37.7, 38.9, 40.1, 41.3, 42.6, 43.8], //p = 0.05
+        checkValue = num => function() {
+            this.value = +this.value <= num ? num : this.value;
+        },
+        findMin = arr => {
+            let min = arr[0];
+            for (let i = 1; i < arr.length; i++) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+            return min;
+        },
+        findMax = arr => {
+            let max = arr[0];
+            for (let i = 1; i < arr.length; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+            return max;
+        },
+        getErf = x => .5 * math.erf(x / 2 ** 0.5),
+        calc = data => {
+            let grpscount = +groups.value,
+                acc = +accuracy.value,
+                minbs = +minobs.value,
+                min = findMin(data),
+                max = findMax(data),
+                frequencies = [],
+                tableOfFrequencies = [`<table class="t"><tr><td><b>X<sub>i</sub></b></td>`, ``, `</tr><tr><td>Частота <b>n<sub>i</sub></b></td>`, ``, `</tr></table><br>`],
+                tableOfFullFrequencies = [`<table class="t"><tr><td><b>X<sub>i</sub></b></td>`, ``, `</tr><tr><td>Частота <b>f<sub>i</sub></b></td>`, ``, `</tr><tr><td>Относительная частота <b>w = f<sub>i</sub>/n</b></td>`, ``, `</tr><tr><td>Плотность относительной частоты <b>w<sub>i</sub>/(X<sub>i+1</sub>-X<sub>i</sub>)</b></td>`, ``, `</tr></table><br>`],
+                t3 = [`<table class="t"><tr><td>Интервалы<br><b>X<sub>i</sub>;X<sub>i+1</sub></b></td><td>Частота<br><b>m<sub>i</sub></b></td><td><b>Z<sub>i</sub> = (X<sub>i</sub>-X<sub>в</sub>)/σ<sub>в</sub></b></td><td><b>Z<sub>i+1</sub> = (X<sub>i+1</sub>-X<sub>в</sub>)/σ<sub>в</sub></b></td><td><b>Ф(Z<sub>i</sub>)</b></td><td><b>Ф(Z<sub>i+1</sub>)</b></td><td><b>P<sub>i</sub></b></td><td><b>nP<sub>i</sub></b></td><td><b>χ<sup>2</sup></b></td></tr>`, ``, `</table>`],
+                grps = [min], //χ
+                integrate = (p1, p2, f) => {
+                    frequencies[p1] = (frequencies[p1 - +!!f] || 0) + (frequencies[p2] || 0);
+                    frequencies.splice(f ? p1 - 1 : p2, 1);
+                    grps.splice(p2, 1);
                 },
-                findMin = arr => {
-                    let min = arr[0];
-                    for (let i = 1; i < arr.length; i++) {
-                        if (arr[i] < min) {
-                            min = arr[i];
+                h = (max - min) / grpscount,
+                l = data.length,
+                res = `<b>X<sub>min</sub></b> = ${min}<br><b>X<sub>max</sub></b> = ${max}<br><b>h</b> = ${h.toFixed(acc)}<br><b>n</b> = ${l}<br><br>`;
+            for (let i = 1; i <= grpscount; i++) {
+                grps[i] = grps[i - 1] + h;
+            }
+            for (let i = 0; i < data.length; i++) {
+                for (let j = 0; j < grpscount; j++) {
+                    if (data[i] >= grps[j] && data[i] < grps[j + 1] || (j == grpscount - 1 && data[i] >= grps[j + 1])) {
+                        frequencies[j] = ++frequencies[j] || 1;
+                        break;
+                    }
+                }
+            }
+            for (let i = 1; i <= grpscount; i++) {
+                tableOfFrequencies[1] += `<td>${grps[i - 1].toFixed(acc)}-${grps[i].toFixed(acc)}</td>`;
+                tableOfFrequencies[3] += `<td>${frequencies[i - 1] || 0}</td>`;
+            }
+            res += tableOfFrequencies.join(``);
+            for (let i = 0; i < frequencies.length; i++) {
+                for (let j = i + 1; j < frequencies.length; j++) {
+                    if ((frequencies[i] || 0) < minbs) {
+                        integrate(i, j--);
+                        if (frequencies[j] < minbs && j == frequencies.length - 1) {
+                            integrate(i, j, 1);
                         }
-                    }
-                    return min;
-                },
-                findMax = arr => {
-                    let max = arr[0];
-                    for (let i = 1; i < arr.length; i++) {
-                        if (arr[i] > max) {
-                            max = arr[i];
+                    } else {
+                        if (j == frequencies.length - 1 && frequencies[j] < minbs) {
+                            integrate(i, j);
                         }
+                        break;
                     }
-                    return max;
-                },
-                getErf = x => .5 * math.erf(x / 2 ** 0.5),
-                calc = data => {
-                    let grpscount = +groups.value,
-                        acc = +accuracy.value,
-                        minbs = +minobs.value,
-                        min = findMin(data),
-                        max = findMax(data),
-                        frequencies = [],
-                        tableOfFrequencies = [`<table class="t"><tr><td><b>X<sub>i</sub></b></td>`, ``, `</tr><tr><td>Частота <b>n<sub>i</sub></b></td>`, ``, `</tr></table><br>`],
-                        tableOfFullFrequencies = [`<table class="t"><tr><td><b>X<sub>i</sub></b></td>`, ``, `</tr><tr><td>Частота <b>f<sub>i</sub></b></td>`, ``, `</tr><tr><td>Относительная частота <b>w = f<sub>i</sub>/n</b></td>`, ``, `</tr><tr><td>Плотность относительной частоты <b>w<sub>i</sub>/(X<sub>i+1</sub>-X<sub>i</sub>)</b></td>`, ``, `</tr></table><br>`],
-                        t3 = [`<table class="t"><tr><td>Интервалы<br><b>X<sub>i</sub>;X<sub>i+1</sub></b></td><td>Частота<br><b>m<sub>i</sub></b></td><td><b>Z<sub>i</sub> = (X<sub>i</sub>-X<sub>в</sub>)/σ<sub>в</sub></b></td><td><b>Z<sub>i+1</sub> = (X<sub>i+1</sub>-X<sub>в</sub>)/σ<sub>в</sub></b></td><td><b>Ф(Z<sub>i</sub>)</b></td><td><b>Ф(Z<sub>i+1</sub>)</b></td><td><b>P<sub>i</sub></b></td><td><b>nP<sub>i</sub></b></td><td><b>χ<sup>2</sup></b></td></tr>`, ``, `</table>`],
-                        grps = [min], //χ
-                        integrate = (p1, p2, f) => {
-                            frequencies[p1] = (frequencies[p1 - +!!f] || 0) + (frequencies[p2] || 0);
-                            frequencies.splice(f ? p1 - 1 : p2, 1);
-                            grps.splice(p2, 1);
-                        },
-                        h = (max - min) / grpscount,
-                        l = data.length,
-                        res = `<b>X<sub>min</sub></b> = ${min}<br><b>X<sub>max</sub></b> = ${max}<br><b>h</b> = ${h.toFixed(acc)}<br><b>n</b> = ${l}<br><br>`;
-                    for (let i = 1; i <= grpscount; i++) {
-                        grps[i] = grps[i - 1] + h;
-                    }
-                    for (let i = 0; i < data.length; i++) {
-                        for (let j = 0; j < grpscount; j++) {
-                            if (data[i] >= grps[j] && data[i] < grps[j + 1] || (j == grpscount - 1 && data[i] >= grps[j + 1])) {
-                                frequencies[j] = ++frequencies[j] || 1;
-                                break;
-                            }
-                        }
-                    }
-                    for (let i = 1; i <= grpscount; i++) {
-                        tableOfFrequencies[1] += `<td>${grps[i - 1].toFixed(acc)}-${grps[i].toFixed(acc)}</td>`;
-                        tableOfFrequencies[3] += `<td>${frequencies[i - 1] || 0}</td>`;
-                    }
-                    res += tableOfFrequencies.join(``);
-                    for (let i = 0; i < frequencies.length; i++) {
-                        for (let j = i + 1; j < frequencies.length; j++) {
-                            if ((frequencies[i] || 0) < minbs) {
-                                integrate(i, j--);
-                                if (frequencies[j] < minbs && j == frequencies.length - 1) {
-                                    integrate(i, j, 1);
-                                }
-                            } else {
-                                if (j == frequencies.length - 1 && frequencies[j] < minbs) {
-                                    integrate(i, j);
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    for (let i = 0; i < frequencies.length; i++) {
-                        let relativeFrequency = frequencies[i] / l,
-                            relativeFrequencyDensity = relativeFrequency / (grps[i + 1] - grps[i]);
-                        tableOfFullFrequencies[1] += `<td>${grps[i].toFixed(acc)}-${grps[i + 1].toFixed(acc)}</td>`;
-                        tableOfFullFrequencies[3] += `<td>${frequencies[i]}</td>`;
-                        tableOfFullFrequencies[5] += `<td>${relativeFrequency.toFixed(acc)}</td>`;
-                        tableOfFullFrequencies[7] += `<td>${relativeFrequencyDensity.toFixed(acc)}</td>`;
-                    }
-                    res += tableOfFullFrequencies.join(``);
-                    let xv = data.reduce((sum, e) => sum + e) / l,
-                        vd = data.reduce((sum, e) => sum + e ** 2, 0) / l - xv ** 2,
-                        vsd = vd ** 0.5,
-                        ivd = l / (l - 1) * vd,
-                        ivso = ivd ** 0.5,
-                        t = 2;
-                    res += `<table class="t"><tr><td>Выборочная средняя <b>X<sub>в</sub></b></td><td>Выборочная дисперсия <b>σ<sup>2</sup><sub>в</sub></b></td><td>Выборочное среднеквадратическое отклонение <b>σ<sub>в</sub></b></td><td>Исправленная выборочная дисперсия <b>σ<sup>2</sup><sub>в испр.</sub></b></td><td>Исправленное выборочное среднеквадратическое отклонение <b>σ<sub>в испр.</sub></b></td></tr><tr><td>${xv.toFixed(acc)}</td><td>${vd.toFixed(acc)}</td><td>${vsd.toFixed(acc)}</td><td>${ivd.toFixed(acc)}</td><td>${ivso.toFixed(acc)}</td></tr></table><p class="h">Доверительный интервал</p>J<sub>β</sub> = (X<sub>в</sub>-t<sub>β</sub>∙(σ<sub>в</sub>/√n);X<sub>в</sub>+t<sub>β</sub>∙(σ<sub>в</sub>/√n))<br>J<sub>β</sub> = (${(xv - t * vsd / l ** 0.5).toFixed(acc)};${(xv + t * vsd / l ** 0.5).toFixed(acc)})<br><br>`;
-                    let x2sum = 0;
-                    for (let i = 0; i < frequencies.length; i++) {
-                        let s = `<span style="font-family:Verdana;font-size:12px">∞</span>`,
-                            zi = (grps[i] - xv) / vsd,
-                            zi1 = (grps[i + 1] - xv) / vsd,
-                            fzi = !i ? -.5 : getErf(zi),
-                            fzi1 = i == frequencies.length - 1 ? 0.5 : getErf(zi1),
-                            pi = fzi1 - fzi,
-                            npi = l * pi,
-                            x2 = (frequencies[i] - npi) ** 2 / npi;
-                        x2sum += x2;
-                        t3[1] += `<tr><td>${grps[i].toFixed(acc)}-${grps[i + 1].toFixed(acc)}</td><td>${frequencies[i]}</td><td>${!i ? `-${s}` : zi.toFixed(acc)}</td><td>${i == frequencies.length - 1 ? `+${s}` : zi1.toFixed(acc)}</td><td>${fzi.toFixed(acc)}</td><td>${fzi1.toFixed(acc)}</td><td>${pi.toFixed(acc)}</td><td>${npi.toFixed(acc)}</td><td>${x2.toFixed(acc)}</td></tr>`;
+                }
+            }
+            for (let i = 0; i < frequencies.length; i++) {
+                let relativeFrequency = frequencies[i] / l,
+                    relativeFrequencyDensity = relativeFrequency / (grps[i + 1] - grps[i]);
+                tableOfFullFrequencies[1] += `<td>${grps[i].toFixed(acc)}-${grps[i + 1].toFixed(acc)}</td>`;
+                tableOfFullFrequencies[3] += `<td>${frequencies[i]}</td>`;
+                tableOfFullFrequencies[5] += `<td>${relativeFrequency.toFixed(acc)}</td>`;
+                tableOfFullFrequencies[7] += `<td>${relativeFrequencyDensity.toFixed(acc)}</td>`;
+            }
+            res += tableOfFullFrequencies.join(``);
+            let xv = data.reduce((sum, e) => sum + e) / l,
+                vd = data.reduce((sum, e) => sum + e ** 2, 0) / l - xv ** 2,
+                vsd = vd ** 0.5,
+                ivd = l / (l - 1) * vd,
+                ivso = ivd ** 0.5,
+                t = 2;
+            res += `<table class="t"><tr><td>Выборочная средняя <b>X<sub>в</sub></b></td><td>Выборочная дисперсия <b>σ<sup>2</sup><sub>в</sub></b></td><td>Выборочное среднеквадратическое отклонение <b>σ<sub>в</sub></b></td><td>Исправленная выборочная дисперсия <b>σ<sup>2</sup><sub>в испр.</sub></b></td><td>Исправленное выборочное среднеквадратическое отклонение <b>σ<sub>в испр.</sub></b></td></tr><tr><td>${xv.toFixed(acc)}</td><td>${vd.toFixed(acc)}</td><td>${vsd.toFixed(acc)}</td><td>${ivd.toFixed(acc)}</td><td>${ivso.toFixed(acc)}</td></tr></table><p class="h">Доверительный интервал</p>J<sub>β</sub> = (X<sub>в</sub>-t<sub>β</sub>∙(σ<sub>в</sub>/√n);X<sub>в</sub>+t<sub>β</sub>∙(σ<sub>в</sub>/√n))<br>J<sub>β</sub> = (${(xv - t * vsd / l ** 0.5).toFixed(acc)};${(xv + t * vsd / l ** 0.5).toFixed(acc)})<br><br>`;
+            let x2sum = 0;
+            for (let i = 0; i < frequencies.length; i++) {
+                let s = `<span style="font-family:Verdana;font-size:12px">∞</span>`,
+                    zi = (grps[i] - xv) / vsd,
+                    zi1 = (grps[i + 1] - xv) / vsd,
+                    fzi = !i ? -.5 : getErf(zi),
+                    fzi1 = i == frequencies.length - 1 ? 0.5 : getErf(zi1),
+                    pi = fzi1 - fzi,
+                    npi = l * pi,
+                    x2 = (frequencies[i] - npi) ** 2 / npi;
+                x2sum += x2;
+                t3[1] += `<tr><td>${grps[i].toFixed(acc)}-${grps[i + 1].toFixed(acc)}</td><td>${frequencies[i]}</td><td>${!i ? `-${s}` : zi.toFixed(acc)}</td><td>${i == frequencies.length - 1 ? `+${s}` : zi1.toFixed(acc)}</td><td>${fzi.toFixed(acc)}</td><td>${fzi1.toFixed(acc)}</td><td>${pi.toFixed(acc)}</td><td>${npi.toFixed(acc)}</td><td>${x2.toFixed(acc)}</td></tr>`;
             }
             let k = frequencies.length - 1 - 2,
                 x2c = tablex2[k - 1];
             res += t3.join(``) + `χ<sup>2</sup> = ${x2sum.toFixed(acc)}<br>Вычисляем число степеней свободы K = S - 1 - r, где<br>S - число интервалов<br>r - число параметров распределения<br>S = ${frequencies.length}<br>r = 2<br>K = ${frequencies.length} - 1 - 2 = ${k}<br>Имя число степеней свободы K = ${k} и задавшись уровнем значимости q = 5%, находим χ<sup>2</sup> критическое (табличное).<br>χ<sup>2</sup><sub>${k};0.05</sub> = ${x2c}<br>χ<sup>2</sup> наблюдаемое ${x2sum < x2c ? `<` : `>`} χ<sup>2</sup> критическое - гипотеза ${x2sum < x2c ? `принимается` : `отвергается`}.`;
             return {
                 txtData: res.replace(/(\d)\./g, `$1,`),
-                data: {grps}
+                data: {
+                    grps
+                }
             };
         },
         f = () => {
             let d1 = new Function(``, data.value)(),
                 d2 = new Function(``, data2.value)(),
-                acc = +accuracy.value, s, res,
+                acc = +accuracy.value,
+                s, res,
                 r1 = calc(d1),
                 r2 = calc(d2),
                 field = [],
@@ -169,18 +172,18 @@
                 }
                 correlationField[5] += `<tr><td>fiy</td><td></td>`;
                 for (let i = 0; i < r2.data.grps.length - 1; i++) {
-                    correlationField[5] += `<td colspan="2">${ygs[i]}</td>`; 
+                    correlationField[5] += `<td colspan="2">${ygs[i]}</td>`;
                 }
                 correlationField[5] += `<td>${l}</td></tr>`;
                 let calculations = [``, ``, ``, ``, ``, ``, ``],
-                    xiyi = 0, 
-                     xii = 0, 
-                     yii = 0, 
-                     xi2 = 0, 
-                     yi2 = 0,
-                     xi_2 = 0,
-                     yi_2 = 0,
-                     correlationCoefficient;
+                    xiyi = 0,
+                    xii = 0,
+                    yii = 0,
+                    xi2 = 0,
+                    yi2 = 0,
+                    xi_2 = 0,
+                    yi_2 = 0,
+                    correlationCoefficient;
                 for (let i = 0; i < r1.data.grps.length - 1; i++) {
                     let a = xgs[i] * xi[i];
                     xii += a;
@@ -242,7 +245,8 @@
             }
             localStorage.setItem(`d1`, data.value);
             localStorage.setItem(`d2`, data2.value);
-        }, run = () => {
+        },
+        run = () => {
             try {
                 f();
             } catch (e) {
